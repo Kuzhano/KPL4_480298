02b2c4f8f28804316f24a4b6434e5f34cbe3a495
kuzhano <sw30243@gmail.com>
2026-03-15 21:56:11 +0700

    second commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        KodePos kp = new KodePos();
+
+        Console.Write("Masukkan kelurahan: ");
+        string kelurahan = Console.ReadLine();
+
+        Console.WriteLine("Kode Pos: " + kp.getKodePos(kelurahan));
+    }
+}
